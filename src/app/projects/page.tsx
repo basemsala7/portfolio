91f2abf00a669f2961b3f3ac06b06c77c767c2fb
basemsala7 <basembebo809@github.com>
2024-03-15 12:09:48 +0200

    import

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,3 @@
-import Header from "../../components/header/Header";
-import Pagination from "../../components/pagination/pagination";
-import ProjectCard from "../../components/projectCard/projectCard";
-import "./projects.css";
-import { dataType } from "../../utli/types";
-
 const Page = async () => {
   return <></>;
 };
