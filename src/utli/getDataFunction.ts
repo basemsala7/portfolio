be0c5aec2030f3ec7d148f910ec577d03218f376
basemsala7 <basembebo809@github.com>
2024-03-17 15:44:39 +0200

    domain name

--- src/utli/getDataFunction.ts
+++ src/utli/getDataFunction.ts
@@ -1,6 +1,8 @@
 export const getProjects = async (page: string) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/projects/${page}`);
+    const res = await fetch(
+      `https://basem-portfolio.netlify.app/api/projects/${page}`
+    );
     if (!res.ok) throw Error("erro ");
     const data = await res.json();
     return data.projects;
