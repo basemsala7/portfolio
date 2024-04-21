85920bad6bf7483cac8f1458fa83698cd49dcb54
basemsala7 <basembebo809@github.com>
2024-04-21 15:58:46 +0200

    env file

--- src/utli/getDataFunction.ts
+++ src/utli/getDataFunction.ts
@@ -1,8 +1,6 @@
 export const getProjects = async (page: string) => {
   try {
-    const res = await fetch(
-      `https://basem-portfolio.netlify.app/api/projects/${page}`
-    );
+    const res = await fetch(`${process.env.DOMAIN}/api/projects/${page}`);
     if (!res.ok) throw Error("erro ");
     const data = await res.json();
     return data.projects;
