4b1052d7aaae2e7e79ab20d017bcc43f0998b65e
basemsala7 <basembebo809@github.com>
2024-04-22 19:34:57 +0200

    loading pages

--- src/app/projects/[page]/page.tsx
+++ src/app/projects/[page]/page.tsx
@@ -4,6 +4,7 @@ import { getProjects } from "../../../utli/getDataFunction";
 import { dataType } from "../../../utli/types";
 export default async function Page({ params }: { params: { page: string } }) {
   const projects = await getProjects(params.page);
+  console.log(params.page, "page param ");
   return (
     <div className="projectContainer">
       {projects?.map((ele: dataType) => (
