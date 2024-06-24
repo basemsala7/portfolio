4e593fde956f4fa274f11953d4fb0cd58a708579
basemsala7 <basembebo809@github.com>
2024-06-24 21:53:52 +0300

    projects error defult params

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,3 +3,4 @@ const Page = async () => {
 };
 
 export default Page;
+  
--- src/utli/getDataFunction.ts
+++ src/utli/getDataFunction.ts
@@ -1,4 +1,4 @@
-export const getProjects = async (page: string) => {
+export const getProjects = async (page: string = "1") => {
   try {
     const res = await fetch(`${process.env.DOMAIN}/api/projects/${page}`);
     if (!res.ok) throw Error("erro ");
